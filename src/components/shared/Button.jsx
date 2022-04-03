0264242ecb889b28d7c81d314e358bc1adf55940
Stefan474 <Fanste474@gmail.com>
2022-04-03 23:01:16 +0200

    These changes broke the program

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,16 @@
+
+function Button({children, version, type, isDisabled}) {
+  return (
+    <Button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+        {children}
+    </Button>
+  )
+}
+
+Button.defaultProps = {
+    version: 'primary',
+    type: 'button',
+    isDisabled: false
+}
+
+export default Button

e1c817486be895fd748fbcd1648ff0cb817ba65a
Stefan474 <Fanste474@gmail.com>
2022-04-05 21:14:27 +0200

    Added conditionally disabled button + started working on rating

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,12 +1,30 @@
 import Card from "./shared/Card"
 import { useState } from "react"
 import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
 function FeedbackForm() {
 
     const[text, setText] = useState ("");
+    const[rating, setRating] = useState (10);
+    const[btnDisabled, setBtnDisabled] = useState (true);
+    const[message, setMessage] = useState ("");
 
     const handleTextChange = (e) => {
+
+        if(text === '') {
+            setBtnDisabled(true);
+            setMessage(null);
+        }
+        else if(text.trim().length <= 10 && text !== '') {
+            
+            setMessage("text must be at least 10 characters long"); 
+            setBtnDisabled(true);
+        }
+        else{
+            setBtnDisabled(false);
+            setMessage(null);
+        }
 
         setText(e.target.value);
     }
@@ -16,11 +34,15 @@ function FeedbackForm() {
     <Card reverse={false}>
         <form>
             <h2>How would you rate your service with us?</h2>
-            {/* todo- rating select component */}
+            <RatingSelect select={(rating) => {
+                  return setRating(rating);
+              }}/>
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-                <Button type="submit" version='secondary'>Send</Button>
+                <Button type="submit" version='secondary' isDisabled={btnDisabled}>Send</Button>
             </div>
+
+            {message && <div className="message">{message}</div>}
         </form>
     </Card>
   )
